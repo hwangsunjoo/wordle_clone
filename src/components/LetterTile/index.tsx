653790c7fdaf3a319641b219d70f6b9f3633453f
Sunjoo Hwang <hwang.sunjoo.dev@gmail.com>
2024-06-29 20:03:23 +0900

    Add structured directories and implement boilerplate codes

---
+++ src/components/LetterTile/index.tsx
@@ -0,0 +1,12 @@
+
+interface LetterTileProps {}
+
+const LetterTile = ({
+}: LetterTileProps) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export type { LetterTileProps };
+export default LetterTile;
